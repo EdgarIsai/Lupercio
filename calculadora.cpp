ddeddbd74a842f65deeabe5d7d4be5c4cf385fad
Edgar Isai <31711689+EdgarIsai@users.noreply.github.com>
2019-08-15 11:58:53 -0500

    Update calculadora.cpp

--- calculadora.cpp
+++ calculadora.cpp
@@ -12,21 +12,33 @@ int *division (int*,int*);
 
 int main()
 {
-    system("cls");
-    cout << "Ingrese numero 1: ";
-    cin >> n1;
-    cout << "Ingrese numero 2: ";
-    cin >> n2;
+    int opc=0;
+    do {
+        system("cls");
+        cout << "Menu" << endl;
+        cout << "1) Suma" << endl;
+        cout << "2) Resta" << endl;
+        cout << "3) Multiplicacion" << endl;
+        cout << "4) Division" << endl;
+        cout << "Seleccione una opcion" << endl;
+        cin >> opc;
+        system("cls");
+        cout << "Ingrese numero 1: ";
+        cin >> n1;
+        cout << "Ingrese numero 2: ";
+        cin >> n2;
 
-    ptr1 =& n1;
-    ptr2 =& n2;
-
-    cout << "La suma es: " << *suma(ptr1,ptr2) << endl;
-    cout << "La resta es: " << *resta(ptr1,ptr2) << endl;
-    cout << "La multiplicacion es: " << *multiplicacion(ptr1,ptr2) << endl;
-    cout << "La division es: " << *division(ptr1,ptr2) << endl;
-
-
+        ptr1 =& n1;
+        ptr2 =& n2;
+        switch(opc) {
+            case 1: cout << "La suma es: " << *suma(ptr1,ptr2) << endl; break;
+            case 2: cout << "La resta es: " << *resta(ptr1,ptr2) << endl;; break;
+            case 3: cout << "La multiplicacion es: " << *multiplicacion(ptr1,ptr2) << endl;; break;
+            case 4: cout << "La division es: " << *division(ptr1,ptr2) << endl;; break;
+        }
+        cout << "Continuar [1] Salir[2]" <<endl;
+        cin >> opc;
+    } while(opc!=2);
     system("pause");
     return 0;
 }
