12a91ec294a44bb6d3b13bb5ad95b1c20c6147e8
Edgar Isai <31711689+EdgarIsai@users.noreply.github.com>
2019-09-04 23:42:25 -0500

    Update decimal-binario.cpp

--- decimal-binario.cpp
+++ decimal-binario.cpp
@@ -1,11 +1,46 @@
 #include <iostream>
 #include<stdio.h>
 #include<stdlib.h>
+#include <cctype>
 
 using namespace std;
 
+int *validationD(string *n){
+    string str = *n;
+    for (char c : str) {
+        if(!isdigit(c)) {
+            int nop=2, *nop_;
+            nop_ =& nop;
+            cout << "Invalido" << endl;
+            return nop_;
+        };
+    };
+
+}
+
+int *validationB(string *n){
+    string str = *n;
+    for (char c : str) {
+        if(c == *"0" || c == *"1") {
+            continue;
+        } else {
+            int nop=2, *nop_;
+            nop_ =& nop;
+            cout << "Invalido" << endl;
+            return nop_;};
+    };
+
+}
+
 int *pow(int *number, int power) {
     int n = *number;
+
+    if (power == 0) {
+        int *p, pw=power+1;
+        p =& pw;
+        return p;
+    }
+
     for (power; power > 1; power--) {
         n *= *number;
     }
@@ -13,17 +48,23 @@ int *pow(int *number, int power) {
     return res;
 }
 
-int *decimalToBin(int *number) {
-    int binary_conv[20];
+int *decimalToBin(string *number) {
+    int f = *validationD(number);
+    if (f==2) {
+        return 0;
+    }
+    string extra_ = *number;
+    int n = stoi(extra_);
+    int binary_conv[500];
     int position=0, co=0, res=0;
-    if (*number == 1) {
+    if (n == 1) {
         cout << *number;
         return 0;
     }
-    while (*number/2 >= 1) {
-        co = *number/2;
-        res = *number%2;
-        *number = co;
+    while (n/2 >= 1) {
+        co = n/2;
+        res = n%2;
+        n = co;
         binary_conv[position] = res;
 
         position++;
@@ -49,7 +90,7 @@ int *decimalToBin(int *number) {
     return 0;
 }
 
-*decimalOctal (int *number) {
+int *decimalOctal (int *number) {
     int remainders[20];
     int *base, bas=8, remainder, position=0;
     base =& bas;
@@ -66,7 +107,7 @@ int *decimalToBin(int *number) {
     return 0;
 }
 
-*octalDecimal (int *number) {
+int *octalDecimal (int *number) {
     int *base, bas=8, remainder, position=0, last=0, lenght=*number, res=0;
     string digit;
     base =& bas;
@@ -111,18 +152,58 @@ int *decimalHexa(int *number) {
     for (;position>=0; position--)
         cout << result[position];
 
+    return 0;
+
+}
+int *hexaDecimal(string *number) {
+    string hexa[2];
+    int len, result=0, *base, bas=16, number_=0, *len_;
+    base =& bas;
+    hexa[0] = number[0];
+    len = hexa[0].size();
+
+    string c;
+
+    for (int i=0; hexa[0][i] != '\0'; i++) {
+
+        len--;
+
+        if(hexa[0][i] >= '0' && hexa[0][i] <= '9')
+            result = hexa[0][i] - 48;
+
+            else if(hexa[0][i] >= 'a' && hexa[0][i] <= 'f')
+                result = hexa[0][i] - 87;
+
+                else if(hexa[0][i] >= 'A' && hexa[0][i] <= 'F')
+                    result = hexa[0][i] - 55;
+
+                    else {
+                        cout << "Numero Invalido" << endl;
+                        return 0;
+                    }
+
+        len_ =& len;
+        number_ += result * *pow(base, len);
+    }
+    cout << number_ << endl;
+    return 0;
+
 }
 
 int *binaryToDecimal(int *number) {
     int position=0, res=0, last=0, lenght=*number, power=2, *powPoint;
     powPoint =& power;
+
+
     do {
         lenght /= 10;
         last++;
     } while(lenght);
     last--;
+
     string str = to_string(*number);
     for (last; last >= 0; last--) {
+
         if (str[last] == *"1" && position == 0) {
             res += 1;
         } else if(str[last] == *"1") {
@@ -139,12 +220,42 @@ int *binaryToDecimal(int *number) {
     return result;
 }
 
+void menu() {
+    cout << "Escoge tu opcion:" << endl;
+    cout << "1) Decimal-Binario" << endl;
+    cout << "2) Binario-Decimal" << endl;
+    cout << "3) Decimal-Octal" << endl;
+    cout << "4) Octal-Decimal" << endl;
+    cout << "5) Decimal-Hexadecimal" << endl;
+    cout << "6) Hexadecimal-Decimal" << endl;
+
+    int opc, com, *com_;
+    string complejas, *comp=&complejas;
+    float simples, *simp;
+    cin >> opc;
+    switch(opc) {
+        case 1:
+            cout << "Escribe tu numero decimal" << endl;
+            cin >> complejas;
+            decimalToBin(comp);
+            break;
+        case 2:
+            cout << "Escribe tu numero binario" << endl;
+            cin >> complejas;
+            int v = *validationB(comp);
+            if (v == 2) {
+                break;
+            }
+            com = stoi(complejas);
+            com_ =& com;
+            binaryToDecimal(com_);
+            break;
+    }
+}
+
 int main()
 {
-    int *num, user_num;
-    cin>>user_num;
-    num =& user_num;
-    *decimalHexa(num);
+    menu();
     return 0;
 }
 
